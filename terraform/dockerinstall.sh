69a4c032b19db4b870a832a05296450a1b2b2818
tarundanda147 <154943106+tarundanda147@users.noreply.github.com>
2024-03-09 17:29:30 +0530

    Update dockerinstall.sh

--- terraform/dockerinstall.sh
+++ terraform/dockerinstall.sh
@@ -3,4 +3,4 @@ git config --global user.email "honeythelab147@gmail.com"
 git config --global user.name "tarundanda147"
 git add .
 git commit -m "backup"
-git push https://USERNAME:PASSWORD@github.com/your-repository.git main
+git push https://tarundanda147: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tarundan@github.com/your-repository.git main
